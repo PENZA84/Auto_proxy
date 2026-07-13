b4633a194d943d64f94f95993a09f0c6529c8271
PENZA84 <145829330+PENZA84@users.noreply.github.com>
2026-07-13 22:24:52 +0300

    Update kuaizui.py

--- kuaizui.py
+++ kuaizui.py
@@ -6,11 +6,11 @@ e_sub =[]
 def get_kkzui():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
-    res = requests.get("https://kkzui.com/jd?orderby=modified", headers=headers)
+    res = requests.get("https://kkzui.com/jd?orderby=modified", headers=headers, timeout=10)
     article_url = re.search(
         r'<h2 class="item-heading"><a href="(https://kkzui.com/(.*?)\.html)"', res.text).groups()[0]
     print(article_url)
-    res = requests.get(article_url, headers=headers)
+    res = requests.get(article_url, headers=headers, timeout=10)
     sub_url = re.search(
         r'<p><strong>这是v2订阅地址</strong>：(.*?)</p>', res.text).groups()[0]
     e_sub.append(sub_url)
